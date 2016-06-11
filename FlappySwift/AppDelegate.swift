84b3021e162f0f0fb7416edd47100bb6fbd3b31e
Nils Fischer <n.fischer@viwid.com>
2016-06-11 20:07:47 +0200

    initial commit with some sprites

---
+++ FlappySwift/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  FlappySwift
+//
+//  Created by Nils Fischer on 06.06.16.
+//  Copyright © 2016 iOS Dev Kurs Universität Heidelberg. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        return true
+    }
+
+}
+
---
+++ FlappySwift/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  GameViewController.swift
+//  FlappySwift
+//
+//  Created by Nils Fischer on 06.06.16.
+//  Copyright (c) 2016 iOS Dev Kurs Universität Heidelberg. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class ViewController: UIViewController {
+    
+    var sceneView: SKView {
+        return self.view as! SKView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Configure scene view
+        sceneView.showsFPS = true
+        sceneView.showsNodeCount = true
+        sceneView.showsDrawCount = true
+    
+    }
+
+}
